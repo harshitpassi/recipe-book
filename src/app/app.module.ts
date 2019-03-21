fdea16ca64316759ef014e84f25e3d0704e5e9cb
Harshit Passi <hrshtpassi@gmail.com>
2019-03-22 00:39:30 +0530

    [1] partial initial component structure for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './features/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './features/recipe-book/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
+import { ShoppingListEditorComponent } from './features/shopping-list/shopping-list-editor/shopping-list-editor.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './features/recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditorComponent,
+    HeaderComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
